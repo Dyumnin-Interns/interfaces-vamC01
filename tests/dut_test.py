8c103cff66077e9460ecd4cf7792bd5c53e53951
VamC01 <vamshiyadav330@gmail.com>
2025-06-07 05:26:16 +0000

    bug fixed

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -7,6 +7,7 @@ from cocotb_bus.monitors import BusMonitor
 import os
 
 
+
 def cb_fn(av):
     global exp_val 
     assert exp_val,"******no exp_val*******"
@@ -14,15 +15,18 @@ def cb_fn(av):
     assert av==exp_val.pop(0), "EEEE"
 
 
-@CoverPoint("top.write_address",xf=lambda x, y, z:x, bins=[4,5])
+@CoverPoint("top.write_address",xf=lambda x, y, z,w:x, bins=[4,5])
 
-@CoverPoint("top.read_address",xf=lambda x, y, z:y, bins=[0,1,2,3])
+@CoverPoint("top.read_address",xf=lambda x, y, z,w:y, bins=[0,1,2,3])
 
-@CoverPoint("top.write_data",xf=lambda x, y, z:z, bins=[0,1])
+@CoverPoint("top.a",xf=lambda x, y, z,w:z, bins=[0,1])
 
-@CoverCross("top.cross.wwr",items=['top.write_address','top.read_address','top.write_data'])
+@CoverPoint("top.b",xf=lambda x, y, z,w:w, bins=[0,1])
 
-def dum(write_address,read_address,write_data):
+
+@CoverCross("top.cross.wwr",items=['top.write_address','top.read_address','top.a','top.b'])
+
+def dum_cover(write_address,read_address,a,b):
     pass
 
 @CoverPoint("top.port.wr.current",
@@ -48,7 +52,7 @@ def wr_port_cover(tnx):
 async def dut_test(dut):
     global exp_val
     exp_val=[]
-
+    #print ("from first")
     dut.RST_N.value=1
     await Timer(1,'ns')
     dut.RST_N.value=0
@@ -56,39 +60,49 @@ async def dut_test(dut):
     await RisingEdge(dut.CLK)
     dut.RST_N.value=1
 
-    wd=InputDriver(dut,'',dut.CLK)
+    wd=WriteDriver(dut,'',dut.CLK)
+    rd=ReadDriver(dut,'',dut.CLK)
     #await Timer(10, 'ns')
     IO_monitor(dut,'',dut.CLK,callback=wr_port_cover)
-    OutputDriver(dut,'',dut.CLK,cb_fn)
-    for i in range(300):
-        wdx=random.randint(0,1)
+    out =OutputDriver(dut,'',dut.CLK,sb_callback=cb_fn)
+
+    for i in range(100):
+        a=random.randint(0,1)
+        b=random.randint(0,1)
         wax=random.randint(4,5)
-        rax=random.randint(0,3)
-        x=val(wax,wdx,rax)
-        #exp_val.append(wdx)
-        wd.append(x)
-        exp_val.append(wdx)
-        dum(wax,rax,wdx)
-    #while len(exp_val)>0:
+        rax=3
+        #x=val(wax,wdx,rax)
+        #exp_val.append(a | b)
+        await wd._driver_send(val(4,a,rax))
+        await wd._driver_send(val(5,b,rax))
+        await rd._driver_send(val(0,0,rax))
+        #await out._driver_send(0)
+        #print ("a= ",a)
+        #print ("b= ",b)
+        #print(f"Time = {get_sim_time('ns')} ns")
+        #print ("")
+        exp_val.append(a|b)
+        #print ("exp_array= ",exp_val)
         #await Timer(2,'ns')
-    await Timer(len(exp_val)*2,'ns')
- 
-    #OutputDriver(dut,'',dut.CLK,cb_fn)
+        await out._driver_send(0)
+        dum_cover(wax,rax,a,b)
+    while len(exp_val)>0:
+        await Timer(2,'ns')
+    #await Timer(len(exp_val)*2,'ns')
+
     coverage_db.report_coverage(cocotb.log.info,bins=True)
     coverage_file=os.path.join(os.getenv('RESULT_PATH', "./"),'coverage.xml')
     coverage_db.export_to_xml(filename=coverage_file)
 
-class InputDriver(BusDriver):
+class WriteDriver(BusDriver):
     _signals=['write_rdy','read_rdy','write_address','write_data','write_en','read_address','read_en','read_data']
     def __init__(self,dut,name,clk):
         BusDriver.__init__(self, dut, name, clk)
         self.bus.write_en.value = 0
-        self.bus.read_en.value = 0
+        #self.bus.read_en.value = 0
         self.clk = clk
 
     async def _driver_send(self,value,sync=True):
-        for i in range(random.randint(0, 20)):
-            await RisingEdge(self.clk)
         if self.bus.write_rdy.value!=1:
             await RisingEdge(self.bus.write_rdy)
         self.bus.write_en.value=1
@@ -98,6 +112,20 @@ class InputDriver(BusDriver):
         await RisingEdge(self.clk)
         await NextTimeStep()
         self.bus.write_en.value=0 
+        #print("write!!")
+        #print ("w_add= ",self.bus.write_address.value, " w_data= ",self.bus.write_data.value)
+        #print(f"Time = {get_sim_time('ns')} ns")
+
+
+class ReadDriver(BusDriver):
+    _signals=['write_rdy','read_rdy','write_address','write_data','write_en','read_address','read_en','read_data']
+    def __init__(self,dut,name,clk):
+        BusDriver.__init__(self, dut, name, clk)
+        #self.bus.write_en.value = 0
+        self.bus.read_en.value = 0
+        self.clk = clk
+
+    async def _driver_send(self,value,sync=True):
         if self.bus.read_rdy.value!=1:
             await RisingEdge(self.bus.read_rdy)
         self.bus.read_en.value=1
@@ -106,7 +134,6 @@ class InputDriver(BusDriver):
         await RisingEdge(self.clk)
         await NextTimeStep()
         self.bus.read_en.value=0
-        
 
 class  IO_monitor(BusMonitor):
     _signals =['write_rdy','read_rdy','write_en','write_address','write_data','read_en','read_address']
@@ -138,21 +165,20 @@ class OutputDriver(BusDriver):
         self.bus.read_en.value = 0
         self.clk = clk
         self.callback = sb_callback
-        self._driver_send(0)
+        #self.append(0)
     async def _driver_send(self,value,sync=True):
-        while True:
-            for i in range(random.randint(0, 20)):
-                await RisingEdge(self.clk)
-            if self.bus.read_rdy.value!=1:
-                await RisingEdge(self.bus.read_rdy)
-            self.bus.read_en.value=1
-            #self.bus.data.value= valueawait
-            await ReadOnly()
-            self.callback(self.bus.read_data.value)
-            await RisingEdge(self.clk)
-            await NextTimeStep()
-            self.bus.read_en.value=0        
-
+        if self.bus.read_rdy.value!=1:
+            await RisingEdge(self.bus.read_rdy)
+        self.bus.read_en.value=1
+        #self.bus.data.value= valueawait
+        await ReadOnly()
+        self.callback(self.bus.read_data.value)
+        await RisingEdge(self.clk)
+        await NextTimeStep()
+        self.bus.read_en.value=0
+        #print("read!!")
+        #print ("r_add= ",self.bus.read_address.value, " r_data= ",self.bus.read_data.value)
+        #print(f"Time = {get_sim_time('ns')} ns")
 
 class val:
     def __init__(self,write_address,write_data,read_address):
