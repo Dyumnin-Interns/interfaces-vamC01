dcc291bef4c51a836fb412de9dde02fba22337f6
VamC01 <vamshiyadav330@gmail.com>
2025-06-09 11:18:27 +0000

    final_bugFix

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -5,7 +5,7 @@ from cocotb_bus.drivers import BusDriver
 from cocotb_coverage.coverage import CoverCross, CoverPoint, coverage_db
 from cocotb_bus.monitors import BusMonitor
 import os
-
+from cocotb.utils import get_sim_time
 
 
 def cb_fn(av):
@@ -66,7 +66,7 @@ async def dut_test(dut):
     rd=ReadDriver(dut,'',dut.CLK)
     #await Timer(10, 'ns')
     IO_monitor(dut,'',dut.CLK,callback=wr_port_cover)
-    out =OutputDriver(dut,'',dut.CLK,sb_callback=cb_fn)
+    out =OutputDriver(dut,'',dut.CLK,cb_fn)
 
     for i in range(10):
         a=random.randint(0,1)
@@ -77,19 +77,23 @@ async def dut_test(dut):
         #exp_val.append(a | b)
         await wd._driver_send(val(4,a,rax))
         await wd._driver_send(val(5,b,rax))
-        await rd._driver_send(val(0,0,rax))
+        rd.append(val(0, 0, 2))
+        while True:
+            await RisingEdge(dut.CLK)
+            if dut.read_data.value == 1:
+                break 
+        rd.append(val(0,0,rax))
         #await out._driver_send(0)
         #print ("a= ",a)
         #print ("b= ",b)
         #print(f"Time = {get_sim_time('ns')} ns")
         #print ("")
         exp_val.append(a|b)
-        #print ("exp_array= ",exp_val)
-        #await Timer(2,'ns')
         await out._driver_send(0)
         dum_cover(wax,rax,a,b)
     while len(exp_val)>0:
         await Timer(2,'ns')
+    #await Timer(700,'ns')
     #await Timer(len(exp_val)*2,'ns')
 
     coverage_db.report_coverage(cocotb.log.info,bins=True)
@@ -114,7 +118,7 @@ class WriteDriver(BusDriver):
         await RisingEdge(self.clk)
         await NextTimeStep()
         self.bus.write_en.value=0 
-        #print("write!!")
+       # print("write!!")
         #print ("w_add= ",self.bus.write_address.value, " w_data= ",self.bus.write_data.value)
         #print(f"Time = {get_sim_time('ns')} ns")
 
@@ -128,11 +132,11 @@ class ReadDriver(BusDriver):
         self.clk = clk
 
     async def _driver_send(self,value,sync=True):
-        #for i in range random.randint(0,20):
-         #   await RisingEdge(self.clk)
         if self.bus.read_rdy.value!=1:
             await RisingEdge(self.bus.read_rdy)
         self.bus.read_en.value=1
+        #print ("exit - ",self.bus.read_address.value)
+        #print(f"Time = {get_sim_time('ns')} ns")
         self.bus.read_address.value= value.read_address
         await ReadOnly()
         await RisingEdge(self.clk)
@@ -169,21 +173,21 @@ class OutputDriver(BusDriver):
         self.bus.read_en.value = 0
         self.clk = clk
         self.callback = sb_callback
-        #self.append(0)
     async def _driver_send(self,value,sync=True):
         if self.bus.read_rdy.value!=1:
             await RisingEdge(self.bus.read_rdy)
         self.bus.read_en.value=1
         #self.bus.data.value= valueawait
         await ReadOnly()
-        #print("read!!")
+        #print("read!!")        
+        #print(f"Time = {get_sim_time('ns')} ns")
         #print ("r_add= ",self.bus.read_address.value, " r_data= ",self.bus.read_data.value)
         self.callback(self.bus.read_data.value)
         await RisingEdge(self.clk)
         await NextTimeStep()
         self.bus.read_en.value=0
 
-        #print(f"Time = {get_sim_time('ns')} ns")
+
 
 class val:
     def __init__(self,write_address,write_data,read_address):
